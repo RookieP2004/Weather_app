38bc5eb3965db86625a9cf3ad7d6d0513c016532
Sujal Chandel <116681047+RookieP2004@users.noreply.github.com>
2025-06-06 14:08:52 +0530

    commit message

---
+++ Weather_app/js/script.js
@@ -0,0 +1,36 @@
+async function getData() {
+    var response = await fetch('http://api.weatherapi.com/v1/current.json?key=f9ffe28bafb14ba0b4394732250506&q=Chandigarh&aqi=no').then(response => {
+         console.log(response.status)
+         return response.json();
+
+    })
+    .then(data => {
+      console.log(data.location);
+
+      var location=document.getElementById("Locations")
+      document.getElementById("Locations").innerHTML = data.location.region + ", " + data.location.country;
+
+      var time=document.getElementById("Time")
+      document.getElementById("Time").innerHTML = data.location.localtime.split(" ")[1];
+
+      var location=document.getElementById("Temperature")
+      document.getElementById("Temperature").innerHTML =data.current.temp_c + "°C";
+  
+      var location=document.getElementById("Conditions")
+      document.getElementById("Conditions").innerHTML =data.current.condition.text;
+
+      var location=document.getElementById("Wind")
+      document.getElementById("Wind").innerHTML =data.current.wind_kph + " km/h";
+
+      var location=document.getElementById("Humidity")
+      document.getElementById("Humidity").innerHTML =data.current.humidity + "%";
+
+      var location=document.getElementById("Sun")
+      document.getElementById("Sun").innerHTML =(data.current.uv / 10).toFixed(1) + " h";
+    })
+    .catch(error => {
+      console.error("Failed to fetch weather data:", error);
+    });
+}
+
+getData();
